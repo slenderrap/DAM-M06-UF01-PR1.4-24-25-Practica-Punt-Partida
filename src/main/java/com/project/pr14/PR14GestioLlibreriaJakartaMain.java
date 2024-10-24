b29145224df0b72876be573626ad34f345cab5f5
orirribas2000@gmail.com <orirribas2000@gmail.com>
2024-10-24 07:39:01 +0200

    acabat jakarta pero amb errors

--- src/main/java/com/project/pr14/PR14GestioLlibreriaJakartaMain.java
+++ src/main/java/com/project/pr14/PR14GestioLlibreriaJakartaMain.java
@@ -1,8 +1,10 @@
 package com.project.pr14;
 
+import com.google.gson.JsonParser;
 import jakarta.json.*;
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -51,8 +53,29 @@ public class PR14GestioLlibreriaJakartaMain {
      * @return Llista de llibres o null si hi ha hagut un error en la lectura.
      */
     public List<Llibre> carregarLlibres() {
-        // *************** CODI PRÀCTICA **********************/
-        return null; // Substitueix pel teu
+        List<Llibre> llistaLlibres = new ArrayList<>();
+        try {
+            if (dataFile.exists()){
+
+                JsonReader jr = Json.createReader(new FileReader(dataFile));
+                JsonArray ja = jr.readArray();
+                for (JsonValue valor: ja){
+                    JsonObject jo = (JsonObject) valor;
+                    int id = jo.getInt("id");
+                    String titol = jo.getString("titol");
+                    String autor = jo.getString("autor");
+                    int any = jo.getInt("any");
+                    llistaLlibres.add(new Llibre(id, titol, autor, any));
+
+                }
+
+            }
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return llistaLlibres; // Substitueix pel teu
     }
 
     /**
@@ -63,7 +86,19 @@ public class PR14GestioLlibreriaJakartaMain {
      * @param nouAny Nou any de publicació.
      */
     public void modificarAnyPublicacio(List<Llibre> llibres, int id, int nouAny) {
-        // *************** CODI PRÀCTICA **********************/
+        boolean canvi=false;
+        for (int i=0;i<llibres.size();i++){
+            if (llibres.get(i).getId()==id){
+                llibres.get(i).setAny(nouAny);
+                canvi = true;
+                break;
+            }
+        }
+        if (canvi){
+            System.out.println("S'ha modificat correctament");
+        }else {
+            System.out.println("No s'ha pogut trobar la id");
+        }
     }
 
     /**
@@ -73,7 +108,9 @@ public class PR14GestioLlibreriaJakartaMain {
      * @param nouLlibre Nou llibre a afegir.
      */
     public void afegirNouLlibre(List<Llibre> llibres, Llibre nouLlibre) {
-        // *************** CODI PRÀCTICA **********************/
+
+        llibres.add(nouLlibre);
+        guardarLlibres(llibres);
     }
 
     /**
@@ -83,7 +120,21 @@ public class PR14GestioLlibreriaJakartaMain {
      * @param id Identificador del llibre a esborrar.
      */
     public void esborrarLlibre(List<Llibre> llibres, int id) {
-        // *************** CODI PRÀCTICA **********************/
+        boolean esborrat= false;
+        for (int i =0;i<llibres.size();i++){
+            if (llibres.get(i).getId()==id){
+                llibres.remove(i);
+                esborrat = true;
+                break;
+            }
+        }
+        if (esborrat){
+            guardarLlibres(llibres);
+            System.out.println("S'ha esborrat correctament");
+        }else {
+            System.out.println("No s'ha pogut esborrar");
+        }
+
     }
 
     /**
@@ -92,6 +143,31 @@ public class PR14GestioLlibreriaJakartaMain {
      * @param llibres Llista de llibres a guardar.
      */
     public void guardarLlibres(List<Llibre> llibres) {
-        // *************** CODI PRÀCTICA **********************/
+        JsonArrayBuilder jab = Json.createArrayBuilder();
+
+        for (int i =0;i<llibres.size();i++){
+            JsonObject llibre= Json.createObjectBuilder()
+                    .add("id",llibres.get(i).getId())
+                    .add("titol",llibres.get(i).getTitol())
+                    .add("autor",llibres.get(i).getAutor())
+                    .add("any",llibres.get(i).getAny())
+                    .build();
+            System.out.println(llibres.get(i).getId());
+            System.out.println(llibres.get(i).getTitol());
+            System.out.println(llibres.get(i).getAutor());
+            System.out.println(llibres.get(i).getAny());
+
+            jab.add(llibre);
+        }
+
+        JsonArray ja = jab.build();
+        System.out.println(ja.get(0));
+        try (JsonWriter jw = Json.createWriter(Files.newBufferedWriter(Paths.get(dataFile.getParent(), "llibres_output_jakarta.json")))) {
+            jw.writeArray(ja);
+            System.out.println("S'ha guardat el Json");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
